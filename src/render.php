15c45b5998eb20720be51ece1b931c8212c1e012
Heikki Vihersalo <heikki@vihersalo.fi>
2024-12-11 14:20:25 +0200

    remove boilerplate comment

--- src/render.php
+++ src/render.php
@@ -8,10 +8,6 @@
  *     $content (string): The block default content.
  *     $block (WP_Block): The block instance.
  *
- * Template file will be wrapped with `template-main` block.
- * It is used to keep the same structure for all templates and to avoid code duplication.
- * CSS classes can be passed in WordPress HTML -template which will be added to content area.
- *
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 
