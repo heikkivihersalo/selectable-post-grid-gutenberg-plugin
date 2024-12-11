cc9d6fb3a4088c907ef2891d6fdef40970a4a260
Heikki Vihersalo <heikki@vihersalo.fi>
2024-12-11 13:24:32 +0200

    add filtering and fix behaviour

--- src/edit.js
+++ src/edit.js
@@ -3,11 +3,13 @@
  */
 import { __ } from '@wordpress/i18n';
 import { useBlockProps } from '@wordpress/block-editor';
+import { useState, useEffect } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import { usePosts } from './hooks/usePosts';
+import Controls from './components/Controls';
 import PostItem from './components/PostItem';
 import './editor.scss';
 
@@ -16,9 +18,20 @@ import './editor.scss';
  * @param {Object} props
  * @return {Element} Element to render.
  */
-export default function Edit( { attributes, setAttributes } ) {
-	const { selectedPosts } = attributes;
-	const posts = usePosts();
+export default function Edit( props ) {
+	const {
+		attributes: { selectedPosts },
+		setAttributes,
+		isSelected: blockSelected,
+	} = props;
+	const [ limit, setLimit ] = useState( 3 );
+	const [ search, setSearch ] = useState( '' );
+	const { data, isLoading } = usePosts( {
+		limit,
+		search,
+		selected: selectedPosts,
+		blockSelected,
+	} );
 
 	const handleSelect = ( event, id ) => {
 		event.preventDefault();
@@ -31,15 +44,47 @@ export default function Edit( { attributes, setAttributes } ) {
 	};
 
 	return (
-		<div { ...useBlockProps( { className: 'posts-grid' } ) }>
-			{ posts.map( ( post ) => (
-				<PostItem
-					key={ post.id }
-					post={ post }
-					selected={ selectedPosts.includes( post.id ) }
-					selectCallback={ handleSelect }
-				/>
-			) ) }
+		<div { ...useBlockProps( { className: 'editor-posts-grid' } ) }>
+			{ /**
+			 * !NOTE There is little bit of prop drilling here, but it's fine for this example.
+			 * Could be refactored to use context API or similar if needed and there
+			 * is more components that need to access the same data.
+			 */ }
+			<Controls
+				blockSelected={ blockSelected }
+				search={ search }
+				setSearch={ setSearch }
+				limit={ limit }
+				setLimit={ setLimit }
+			/>
+			{ isLoading && <p>{ __( 'Loading posts...' ) }</p> }
+
+			{ /**
+			 * Handle the empty and loading states
+			 */ }
+			{ ! isLoading && data.length === 0 && (
+				<p>
+					{ __(
+						'No posts selected. Click here to select the posts to show.'
+					) }
+				</p>
+			) }
+
+			{ /**
+			 * If everything is loaded and there are posts to show, render the posts grid.
+			 */ }
+			<div className="posts-grid">
+				{ ! isLoading &&
+					data.map( ( post ) => (
+						<PostItem
+							key={ post.id }
+							post={ post }
+							selected={ selectedPosts.includes( post.id ) }
+							selectCallback={ handleSelect }
+							showSelect={ blockSelected }
+						/>
+					) ) }
+			</div>
 		</div>
 	);
 }
