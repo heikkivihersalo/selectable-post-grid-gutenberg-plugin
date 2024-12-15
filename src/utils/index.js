b4ddf7b87bd62dd08f9c019f0d19000d596e9d2a
Heikki Vihersalo <heikki@vihersalo.fi>
2024-12-15 15:14:54 +0200

    move format date string to utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
+export * from './lib/formatDateString';
 export * from './lib/getPosts';
 export * from './lib/getSelectedPosts';
 export * from './lib/mergePosts';
---
+++ src/utils/lib/formatDateString.js
@@ -0,0 +1,6 @@
+export function formatDateString( ISODate ) {
+    const date = new Date( ISODate );
+    return `${ date.getDate() }.${
+        date.getMonth() + 1
+    }.${ date.getFullYear() }`;
+};
