4041f1c9c25341003d95308b5c0adf7b37042e64
Heikki Vihersalo <heikki@vihersalo.fi>
2024-12-15 15:07:01 +0200

    add tests for handle select

--- src/tests/edit.test.jsx
+++ src/tests/edit.test.jsx
@@ -1,10 +1,8 @@
-/**
- * Dependencies
- */
 import { beforeEach, afterEach, describe, expect, it, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
+import { POSTS } from './__mocks__/posts';
 import Edit from '../edit';
 
 const mocksData = vi.hoisted( () => {
@@ -27,7 +25,6 @@ describe( '#edit', () => {
 		mocksData.usePosts.mockReturnValue( {
             data: [],
             isLoading: false,
-            refetch: vi.fn(),
         } );
 	} );
 
@@ -44,7 +41,6 @@ describe( '#edit', () => {
 		mocksData.usePosts.mockReturnValue( {
             data: [],
             isLoading: true,
-            refetch: vi.fn(),
         } );
 
 		render( <Edit { ...props } /> );
@@ -55,7 +51,6 @@ describe( '#edit', () => {
 		mocksData.usePosts.mockReturnValue( {
             data: [],
             isLoading: false,
-            refetch: vi.fn(),
         } );
 
 		render( <Edit { ...props } /> );
@@ -66,28 +61,57 @@ describe( '#edit', () => {
 
     it( 'should render posts', () => {
         mocksData.usePosts.mockReturnValue( {
-            data: [
-                {
-                    id: 1,
-                    title: { rendered: 'Post 1' },
-                    metadata: {
-                        featured_image: {
-                            url: '',
-                            alt: '',
-                            width: '',
-                            height: '',
-                            title: '',
-                        },
-                        categories: [],
-                    },
-                    date: '2021-01-01T00:00:00',
-                },
-            ],
+            data: POSTS,
             isLoading: false,
-            refetch: vi.fn(),
         } );
 
         render( <Edit { ...props } /> );
         expect( screen.getByText( 'Post 1' ) ).toBeInTheDocument();
     } );
 } );
+
+describe( '#handleSelect', () => {
+    beforeEach( () => {
+		// This is the default mock for the useSelect hook
+        mocksData.usePosts.mockReturnValue( {
+            data: POSTS,
+            isLoading: false,
+        } );
+	} );
+
+	afterEach( () => {
+		vi.restoreAllMocks();
+	} );
+
+    it('should add post to selected posts', () => {
+        const setAttributes = vi.fn();
+        render(<Edit {...{
+            attributes: { selectedPosts: [] },
+            setAttributes,
+            isSelected: true,
+        }} />);
+
+        const selectCheckbox = screen.getAllByTestId('posts-grid-item-select-button')[0];
+        selectCheckbox.click();
+
+        expect(setAttributes).toHaveBeenCalledWith({
+            selectedPosts: [1]
+        });
+    });
+
+    it('should remove post from selected posts', () => {
+        const setAttributes = vi.fn();
+        render(<Edit {...{
+            attributes: { selectedPosts: [1,2] },
+            setAttributes,
+            isSelected: true,
+        }} />);
+
+        const selectCheckbox = screen.getAllByTestId('posts-grid-item-select-button')[0];
+        selectCheckbox.click();
+
+        expect(setAttributes).toHaveBeenCalledWith({
+            selectedPosts: [2]
+        });
+    });
+});
