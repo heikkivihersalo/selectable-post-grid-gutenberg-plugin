cd00c55026e7578ff9658156bd44fa05b18507a2
Heikki Vihersalo <heikki@vihersalo.fi>
2024-12-15 15:06:48 +0200

    initial commit

---
+++ src/tests/__mocks__/posts.js
@@ -0,0 +1,31 @@
+export const POSTS = [
+    {
+        id: 1,
+        title: { rendered: 'Post 1' },
+        metadata: {
+            featured_image: {
+                url: '',
+                alt: '',
+                width: '',
+                height: '',
+                title: '',
+            },
+            categories: [],
+        },
+        date: '2021-01-01T00:00:00',
+    },
+    {
+        id: 2,
+        title: { rendered: 'Post 2' },
+        metadata: {
+            featured_image: {
+                url: '',
+                alt: '',
+                width: '',
+                height: '',
+                title: '',
+            },
+            categories: [],
+        },
+    },
+];
