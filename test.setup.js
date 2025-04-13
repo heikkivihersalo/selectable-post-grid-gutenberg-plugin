0d088641c1fd506bafaa0576c243b84e0cf6e0cf
Heikki Vihersalo <heikki@vihersalo.fi>
2025-04-13 16:08:41 +0300

    remove unnecessary steps

--- test.setup.js
+++ test.setup.js
@@ -1,20 +1,7 @@
-import { afterEach, vi } from 'vitest';
+import { afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 
 // runs a clean after each test case (e.g. clearing jsdom) `important for test isolation`
 afterEach( () => {
 	cleanup();
 } );
-
-// Mock WordPress modules
-vi.mock( '@wordpress/i18n', () => ( {
-	__: ( text, namespace = '' ) => text,
-} ) );
-
-vi.mock( '@wordpress/element', async () => ( {
-	useState: ( state ) => {
-		return [ state, vi.fn() ];
-	},
-	useEffect: vi.fn(),
-} ) );
- 
